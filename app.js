586f3fd3b13af5c61000dfd38f3b19400cf164c4
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-03-24 10:01:14 -1000

    nothing done

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var clicker=document.getElementById("clicker");
+
+ var btn = document.createElement("button");
+  btn.innerHTML = "test";
+  btn.id = "test";
+  btn.addEventListener('click',function(event){
+    console.log;
+  });
+
+  clicker.appendChild(btn);
